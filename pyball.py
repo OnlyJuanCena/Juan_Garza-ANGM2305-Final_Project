f95b86952fafc9e0001ed25f724880997f318954
JuanMAC <jjgarchomp@gmail.com>
2024-11-07 06:02:51 -0600

    Added a screen, a rectangle to be drawn on screen, and a move() function that needs to be tied to the player block

--- pyball.py
+++ pyball.py
@@ -1,18 +1,45 @@
 import pygame
+import sys
 
 WIDTH = 640
 HEIGHT = 480
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+
+BLACK = (0,0,0)
+
+
+class Player_Block:
+
+    def __init__(self, pos=(0,0)):
+        self.pos = pygame.Vector2(pos)
+        self.width = 50
+        self.height = 200
+        self.color = (255,255,255)
+        self.speed = 3.0
+
+        # Rect Properties
+        self.block_controller = pygame.Rect(pos[0], pos[1], self.width, self.height)
+        # Display Rect
+        self.block = pygame.draw.rect(screen, self.color, self.block_controller)
+
+    def move(self, dir):
+        self.pos = self.pos + dir * self.speed
+
+    def draw(self):
+        self.block = pygame.draw.rect(screen, self.color, self.block_controller)
+        
 
 def main():
     # Initialize Pygame
     pygame.init()
     clock = pygame.time.Clock()
-
-    # Create Window
-    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    dt = 0
 
     # Window Title
     pygame.display.set_caption("PyBall: Retro Edition")
+
+    #Create Player
+    player = Player_Block(pos=(WIDTH//2,HEIGHT//2))
 
     # Game loop to keep the window open
     while True:
@@ -29,3 +56,14 @@ def main():
             player.move(pygame.Vector2(-1.5, 0))
         if keys[pygame.K_RIGHT]:
             player.move(pygame.Vector2(1.5, 0))
+        
+        print(player.pos)
+
+        screen.fill(BLACK)
+        player.draw()
+        # Update display
+        pygame.display.flip()
+        dt = clock.tick(30)
+
+if __name__ == "__main__":
+    main()
