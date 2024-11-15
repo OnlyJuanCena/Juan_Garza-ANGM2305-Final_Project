0dc78e2e9c92a99599c683027190f44bfcc425b4
JuanMAC <jjgarchomp@gmail.com>
2024-11-14 21:57:38 -0600

    Fixed AI not going down

--- pyball.py
+++ pyball.py
@@ -37,8 +37,7 @@ class Ball:
         self.pos = pygame.Vector2(pos)
         self.size = 15
         self.color = (255,255,255)
-        self.speed = speed
-        self.x_dir = -1
+        self.x_dir = 1
         self.y_dir = -1
 
         # Ball Controller
@@ -46,9 +45,9 @@ class Ball:
         # Display Ball
         self.block = pygame.draw.rect(screen, self.color, self.block_controller)
 
-    def move(self):
-        self.pos[0] += self.speed * self.x_dir
-        self.pos[1] += self.speed * self.y_dir
+    def move(self, speed):
+        self.pos[0] += speed * self.x_dir
+        self.pos[1] += speed * self.y_dir
 
         self.block_controller = pygame.Rect(self.pos[0], self.pos[1], self.size, self.size)
 
@@ -84,6 +83,7 @@ def main():
         ball.reset()
         player.pos = (WIDTH//16,HEIGHT//2)
         comp.pos = (WIDTH - WIDTH//16 - player.width, HEIGHT//2)
+        speed_multiplier = 3
 
 
     # Game loop to keep the window open
@@ -95,12 +95,12 @@ def main():
 
         screen.fill(BLACK)
 
-        # Keybinds
+        # Player movement
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP] and player.pos[1] > 2:
-            player.move(pygame.Vector2(0, -1.5*speed_multiplier))
+            player.move(pygame.Vector2(0, -1.5))
         if keys[pygame.K_DOWN] and player.pos[1] < HEIGHT-player.height-2:
-            player.move(pygame.Vector2(0, 1.5*speed_multiplier))
+            player.move(pygame.Vector2(0, 1.5))
 
         # Block-hit detection
         if pygame.Rect.colliderect(player.block_controller, ball.block_controller):
@@ -110,13 +110,7 @@ def main():
             ball.x_dir *= -1
             print("Bazzinga")
 
-
-        if (comp.pos[1] <= ball.pos[1] and comp.pos[1] < HEIGHT-player.height-2):
-            comp.move(pygame.Vector2(0, 1.5*speed_multiplier//2))
-        if (comp.pos[1] >= ball.pos[1] and comp.pos[1] > 2):
-            comp.move(pygame.Vector2(0, -1.5*speed_multiplier//2))
-
-        # Edge detection
+        # Ball movement
         if ball.pos[1] <= 0 or ball.pos[1] >= HEIGHT - ball.size:
             ball.y_dir *= -1
         if ball.pos[0] >= WIDTH:
@@ -125,7 +119,18 @@ def main():
         elif ball.pos[0] <= 0 - ball.size:
             time.sleep(1)
             Default_Positions()
-        ball.move()
+        ball.move(speed_multiplier*1.3)
+
+        # AI movement
+        if (comp.pos[1] <= ball.pos[1] and comp.pos[1] < HEIGHT-player.height-2):
+            comp.move(pygame.Vector2(0, 1.5))
+            print("moving down")
+        if (comp.pos[1] >= ball.pos[1] and comp.pos[1] > 2):
+            comp.move(pygame.Vector2(0, -1.5))
+            print("moving up")
+
+        # Ball Speed
+        speed_multiplier *= 1.0005
             
         player.draw()
         comp.draw()
